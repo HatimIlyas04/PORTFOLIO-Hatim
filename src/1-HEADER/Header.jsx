89286b7abd6852b11c757696f98fdf8e14d2a16c
HATIM ILYAS <125820069+HatimIlyas04@users.noreply.github.com>
2024-01-25 17:35:06 +0100

    Update Header.jsx

--- src/1-HEADER/Header.jsx
+++ src/1-HEADER/Header.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   const [showModal, setShowModal] = useState(false)
-  const [theme , setTheme] = useState(localStorage.getItem('currentMode') ?? 'dark')
+  const [theme, setTheme] = useState(localStorage.getItem('currentMode') ?? 'dark')
 
   useEffect(() => {
-      if (theme == 'light') {
+    if (theme == 'light') {
       document.body.classList.remove('dark')
       document.body.classList.add('light')
-      }else{
-        document.body.classList.remove('light')
+    } else {
+      document.body.classList.remove('light')
       document.body.classList.add('dark')
-      }
-  } , [theme])
+    }
+  }, [theme])
 
   const ShowModal = () => {
     setShowModal(true)
@@ -30,18 +30,18 @@ function Header() {
       </button>
       <div />
       <nav>
-        <ul className='flex'>
+        <ul className='flex navbar-links'>
+
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/About'}>About</Link></li>
           <li><Link to={'/projects'} >Projects</Link></li>
-          <li><Link to={'/speaking'}>Speaking</Link></li>
           <li><Link to={'/contact'}>Contact</Link></li>
         </ul>
       </nav>
 
       <button onClick={() => {
         // Send  value to ls
-        localStorage.setItem('currentMode' , theme == 'dark' ? 'light' : 'dark')
+        localStorage.setItem('currentMode', theme == 'dark' ? 'light' : 'dark')
 
         setTheme(localStorage.getItem('currentMode'))
 
@@ -54,14 +54,13 @@ function Header() {
           <ul className='modal '>
             <li>
               <button className='close' onClick={HideModal}>
-              <img width="35" height="35" src="https://img.icons8.com/sf-black-filled/64/FFFFFF/x.png" alt="x"/>
+                <img width="35" height="35" src="https://img.icons8.com/sf-black-filled/64/FFFFFF/x.png" alt="x" />
               </button>
             </li>
-            <li><a href="">About</a></li>
-            <li><a href="">Articles</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">Speaking</a></li>
-            <li><a href="">Uses</a></li>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/About'}>About</Link></li>
+            <li><Link to={'/projects'} >Projects</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
           </ul>
         </div>
       )}
